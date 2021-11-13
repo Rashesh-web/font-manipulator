d2e52131b1d7bd758f9aef3c24e6d87aef7980d3
Rashesh-web <82595271+Rashesh-web@users.noreply.github.com>
2021-11-13 13:46:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-function setup(){
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+function setup() {
     video = createCapture(VIDEO);
-    video.size(550,500);
-    
-    canvas = createCanvas(550,550);
-    canavs.position(560,150);
+    video.size(550, 500);
+  
+    canvas = createCanvas(550, 550);
+    canvas.position(500,120);
+  
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
-}
+  }
 
-function modelLoaded(){
-    console.log("PoseNet is intialized!");
+  function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+  }
+  
+  
+  function gotPoses(results)
+  {
+    if(results.length > 0)
+    {
+      console.log(results);
+      leftWristX = results[0].pose.leftWrist.x;
+      rightWristX = results[0].pose.rightWrist.x;
+      difference = floor(leftWristX - rightWristX);
+    } 
 }
 
 function draw(){
-    backgrouond('#969A97');
-}
-function gotPoses(results){
-    if(results.length > 0 ){
-        console.log(results);
-    }
+    background('#6C91C2');
+    textSize(difference);
+    fill('#FFE787');
+    text('Peter', 50,400 );
 }
